a3c1f0b8b4f99d25ed2091a3c0b48b685b2b6ffe
Shellmaker.io (eboxx group) <30957975+shellmaker@users.noreply.github.com>
2017-11-28 11:17:55 -0200

    commit20171128

---
+++ reset_wsus_id.bat
@@ -0,0 +1,11 @@
+@Echo off
+if exist %systemdrive%\SUSClientID.log goto end
+net stop wuauserv
+net stop bits
+reg delete “HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate” /v PingID /f  > %systemdrive%\SUSClientID.log 2>&1
+reg delete “HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate” /v AccountDomainSid /f  >> %systemdrive%\SUSClientID.log 2>&1
+reg delete “HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate” /v SusClientId /f  >> %systemdrive%\SUSClientID.log 2>&1
+net start wuauserv
+wuauclt.exe /resetauthorization /detectnow         
+:end
+exit
